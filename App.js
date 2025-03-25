550c9c8ab49d84eebae8e426508c771d2b2c808b
Ana Julia <anajuliademattei26@gmailcom>
2025-03-25 15:50:05 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, TextInput, Button, TouchableOpacity, StyleSheet } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>MOBILE</Text>
+            <Text style={styles.description}>Atividade 03 - Componentes básicos</Text>
+            <TextInput placeholder="Insira seu nome aqui!" style={styles.input}></TextInput>
+            <TouchableOpacity style={styles.botao}> Editar </TouchableOpacity>
+            <TouchableOpacity style={styles.botao2}> Enviar </TouchableOpacity>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: 10,
+  },
+  description: {
+    fontSize: 16,
+    color: "#666",
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  input: {
+    width: "70%",
+    height: 40,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    backgroundColor: "#fff",
+  },
+  botao: {
+    backgroundColor: "#FF00FF",
+    color: "#fff",
+    padding: 15, 
+    borderRadius: 90,
+    alignItems: "center",
+    marginBottom: 10,
+    width: "50vh",
+  },
+  botao2: {
+    backgroundColor: "#87b344",
+    padding: 15, 
+    borderRadius: 100,
+    alignItems: "center",
+    width: "50vh",
+    color: "#fff",
   },
 });
